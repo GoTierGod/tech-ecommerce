db32a0fa4c7f3bbbca77db1d8de2ca5d7d9bd046
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-06-12 23:44:22 -0400

    gaming section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { CardProductDetails } from '@/types/products'
 import { getData } from '@/helpers/getData'
 import Offers from '@/components/Offers'
 import Highlighted from '@/components/Highlighted'
+import Gaming from '@/components/Gaming'
 
 export default async function Home() {
     const laptops: CardProductDetails[] = await getData(
@@ -12,7 +13,7 @@ export default async function Home() {
     )
 
     const offers: CardProductDetails[] = await getData(
-        `https://ft-drf-api.vercel.app/api/products?category=smartphones&limit=3`
+        `https://ft-drf-api.vercel.app/api/products?category=monitors&limit=3`
     )
 
     const smartphone: CardProductDetails[] = await getData(
@@ -22,12 +23,19 @@ export default async function Home() {
         `https://ft-drf-api.vercel.app/api/products?category=headphones&limit=1`
     )
 
+    const gaming: CardProductDetails[] = await getData(
+        `https://ft-drf-api.vercel.app/api/products?category=laptops&limit=6`
+    )
+
     return (
         <main className={styles.main}>
             <div>
                 <Offers products={offers} />
                 <ProductRows title='Laptops' products={laptops} url='/' />
                 <Highlighted products={[smartphone[0], headphones[0]]} />
+                <ProductRows title='Laptops' products={laptops} url='/' />
+                <Gaming products={gaming} />
+                <ProductRows title='Laptops' products={laptops} url='/' />
             </div>
         </main>
     )
--- components/Gaming.tsx
+++ components/Gaming.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import style from '../styles/gaming.module.css'
 
 import { CardProductDetails } from '@/types/products'
+import Image from 'next/image'
 
 interface GamingProps {
     products: CardProductDetails[]
@@ -8,7 +10,36 @@ interface GamingProps {
 
 // SECTION SHOWING 6 GAMING PRODUCTS
 const Gaming = ({ products }: GamingProps) => {
-    return <section className={style.section}></section>
+    return (
+        <section className={style.section}>
+            <div className={style.header}>
+                <div>
+                    <span>Desktops - Laptops - Graphic Cards and more..</span>
+                    <h4>The Best Gaming Offers</h4>
+                </div>
+                <Link href='/' className={style.link}>
+                    <span>Gaming Offers</span>
+                </Link>
+            </div>
+            <div className={style.grid}>
+                {products.map(product => (
+                    <Link
+                        key={product.details.id}
+                        href={`/product/${product.details.id}`}
+                        className={style.product}
+                    >
+                        <Image
+                            src={product.image.url}
+                            alt={product.image.description}
+                            width={250}
+                            height={250}
+                            quality='25'
+                        />
+                    </Link>
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default Gaming
