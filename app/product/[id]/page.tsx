48c37c266280be682e7e639b4f9f64914dfb08a3
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-06-16 21:53:50 -0400

    responsive style and structure for the product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -72,160 +72,200 @@ const product = async ({ params }: { params: { id: string } }) => {
 
     const defaultImg = product.images.find(image => image.is_default)
 
+    const content = (
+        <div className={style.content}>
+            {/* ------------------------- OFFER -------------------------  */}
+            <div className={style.offer}>
+                <div>
+                    <span>$ {product.details.price}</span>
+                    <span>Best Seller</span>
+                </div>
+                <span>$ {product.details.offer_price}</span>
+                <span>
+                    Available in{' '}
+                    <span>
+                        {getInstallments(
+                            product.details.installments,
+                            product.details.offer_price
+                        )}
+                    </span>
+                </span>
+            </div>
+            {/* ------------------------- OPTIONS -------------------------  */}
+            <div className={style.options}>
+                <span>stock: {product.details.stock}</span>
+                <Link href='/'>Buy Now</Link>
+                <Link href='/'>Add to Cart</Link>
+            </div>
+            {/* ------------------------- DELIVERY -------------------------  */}
+            <div className={style.delivery}>
+                <h3>Delivery</h3>
+                <span>
+                    <FontAwesomeIcon icon={faTruck} /> It arrives for{' '}
+                    {getDeliveryCost(product.details.price)} this {deliveryDay}
+                </span>
+                <span>
+                    <FontAwesomeIcon icon={faBox} />
+                    Pick up your package for{' '}
+                    {getDeliveryCost(product.details.price)} starting from{' '}
+                    {deliveryDay}
+                </span>
+            </div>
+            {/* ------------------------- WARRANTY AND POINTS -------------------------  */}
+            <div className={style.wp}>
+                <div>
+                    <h3>Warranty</h3>
+                    <span>
+                        <FontAwesomeIcon icon={faShieldAlt} />{' '}
+                        {product.details.months_warranty} Months
+                    </span>
+                    <Link href='/'>Read More</Link>
+                </div>
+                <div>
+                    <h3>FT Points</h3>
+                    <span>
+                        <FontAwesomeIcon icon={faStar} />{' '}
+                        {Number(product.details.price)} Points
+                    </span>
+                    <Link href='/'>Log In</Link>
+                </div>
+            </div>
+            {/* ------------------------- PAYMENT -------------------------  */}
+            <div className={style.payment}>
+                <h3>Payment Methods</h3>
+                <div>
+                    <div>
+                        <h4>Credit Cards</h4>
+                        <span>
+                            {getInstallments(
+                                product.details.installments,
+                                product.details.offer_price
+                            )}
+                        </span>
+                    </div>
+                    <div>
+                        <h4>Others</h4>
+                        <span>$ {product.details.offer_price}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <main>
             <div className={style.wrapper}>
                 <div className={style.product}>
-                    {/* ------------------------- HEADER -------------------------  */}
-                    <div className={style.header}>
-                        <div>
-                            <h2>{product.details.name}</h2>
-                            {Number(product.details.price) > 50 && (
-                                <span>Free Shipping</span>
-                            )}
-                        </div>
-                        <div>
-                            <span>
-                                {getStars(
-                                    product.reviews_counter,
-                                    product.rating
-                                )}
-                            </span>
-                            <span>
-                                (
-                                <Link href='/'>
-                                    {product.reviews_counter} Reviews
-                                </Link>
-                                )
-                            </span>
-                        </div>
-                    </div>
-                    {/* ------------------------- IMAGES -------------------------  */}
-                    <div className={style.images}>
-                        <Image
-                            src={defaultImg?.url || ''}
-                            alt={defaultImg?.description || ''}
-                            width={450}
-                            height={450}
-                            quality='100'
-                        />
-                        <div>
-                            {product.images.map(image => (
-                                <Image
-                                    key={image.id}
-                                    src={image.url}
-                                    alt={image.description}
-                                    height={48}
-                                    width={48}
-                                    quality='100'
-                                />
-                            ))}
-                        </div>
-                    </div>
-                    {/* ------------------------- MORE BRAND PRODUCTS -------------------------  */}
-                    <div className={style.brandProducts}>
-                        <div>
-                            <h3>More of {product.details.brand.name}</h3>
-                        </div>
-                        <div>
-                            {brandProducts
-                                .filter(
-                                    product =>
-                                        product.details.id.toString() !== id
-                                )
-                                .map(product => (
-                                    <VerticalCard
-                                        key={product.details.id}
-                                        product={product}
-                                    />
-                                ))}
-                        </div>
-                    </div>
-                    {/* ------------------------- CATEGORY -------------------------  */}
-                    <div className={style.category}>
-                        <h3>See more in this category</h3>
-                        <Link href='/'>{product.details.category.title}</Link>
-                    </div>
-                    {/* ------------------------- OFFER -------------------------  */}
-                    <div className={style.offer}>
-                        <div>
-                            <span>$ {product.details.price}</span>
-                            <span>Best Seller</span>
-                        </div>
-                        <span>$ {product.details.offer_price}</span>
-                        <span>
-                            Available in{' '}
-                            <span>
-                                {getInstallments(
-                                    product.details.installments,
-                                    product.details.offer_price
-                                )}
-                            </span>
-                        </span>
-                    </div>
-                    {/* ------------------------- OPTIONS -------------------------  */}
-                    <div className={style.options}>
-                        <span>stock: {product.details.stock}</span>
-                        <Link href='/'>Buy Now</Link>
-                        <Link href='/'>Add to Cart</Link>
-                    </div>
-                    {/* ------------------------- DELIVERY -------------------------  */}
-                    <div className={style.delivery}>
-                        <h3>Delivery</h3>
-                        <span>
-                            <FontAwesomeIcon icon={faTruck} /> It arrives for{' '}
-                            {getDeliveryCost(product.details.price)} this{' '}
-                            {deliveryDay}
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faBox} />
-                            Pick up your package for{' '}
-                            {getDeliveryCost(product.details.price)} starting
-                            from {deliveryDay}
-                        </span>
-                    </div>
-                    {/* ------------------------- WARRANTY AND POINTS -------------------------  */}
-                    <div className={style.wp}>
-                        <div>
-                            <h3>Warranty</h3>
-                            <span>
-                                <FontAwesomeIcon icon={faShieldAlt} />{' '}
-                                {product.details.months_warranty} Months
-                            </span>
-                            <Link href='/'>Read More</Link>
-                        </div>
-                        <div>
-                            <h3>FT Points</h3>
-                            <span>
-                                <FontAwesomeIcon icon={faStar} />{' '}
-                                {Number(product.details.price)} Points
-                            </span>
-                            <Link href='/'>Log In</Link>
-                        </div>
-                    </div>
-                    {/* ------------------------- PAYMENT -------------------------  */}
-                    <div className={style.payment}>
-                        <h3>Payment Methods</h3>
-                        <div>
+                    <div className={style.leftContent}>
+                        {/* ------------------------- HEADER -------------------------  */}
+                        <div className={style.header}>
                             <div>
-                                <h4>Credit Cards</h4>
+                                <h2>{product.details.name}</h2>
+                                {Number(product.details.price) > 50 && (
+                                    <span>Free Shipping</span>
+                                )}
+                            </div>
+                            <div>
                                 <span>
-                                    {getInstallments(
-                                        product.details.installments,
-                                        product.details.offer_price
+                                    {getStars(
+                                        product.reviews_counter,
+                                        product.rating
                                     )}
                                 </span>
-                            </div>
-                            <div>
-                                <h4>Others</h4>
-                                <span>$ {product.details.offer_price}</span>
+                                <span>
+                                    (
+                                    <Link href='/'>
+                                        {product.reviews_counter} Reviews
+                                    </Link>
+                                    )
+                                </span>
                             </div>
                         </div>
+                        {/* ------------------------- IMAGES -------------------------  */}
+                        <div className={style.images}>
+                            <Image
+                                src={defaultImg?.url || ''}
+                                alt={defaultImg?.description || ''}
+                                width={450}
+                                height={450}
+                                quality='100'
+                            />
+                            <div>
+                                {product.images.map(image => (
+                                    <Image
+                                        key={image.id}
+                                        src={image.url}
+                                        alt={image.description}
+                                        height={48}
+                                        width={48}
+                                        quality='100'
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                        {/* ------------------------- MORE BRAND PRODUCTS -------------------------  */}
+                        <div className={style.brandProducts}>
+                            <div>
+                                <h3>More of {product.details.brand.name}</h3>
+                            </div>
+                            <div>
+                                {brandProducts
+                                    .filter(
+                                        product =>
+                                            product.details.id.toString() !== id
+                                    )
+                                    .map(product => (
+                                        <VerticalCard
+                                            key={product.details.id}
+                                            product={product}
+                                        />
+                                    ))}
+                            </div>
+                        </div>
+                        {/* ------------------------- CATEGORY -------------------------  */}
+                        <div className={style.category}>
+                            <h3>See more in this category</h3>
+                            <Link href='/'>
+                                {product.details.category.title}
+                            </Link>
+                        </div>
+                        {/* ------------------------- FOR SMALL SCREENS -------------------------  */}
+                        {content}
+                        {/* ------------------------- DESCRIPTION -------------------------  */}
+                        <div className={style.description}>
+                            <h3>Description</h3>
+                            <p>{product.details.description}</p>
+                        </div>
                     </div>
-                    {/* ------------------------- DESCRIPTION -------------------------  */}
-                    <div className={style.description}>
-                        <h3>Description</h3>
-                        <p>{product.details.description}</p>
+                    <div className={style.rightContent}>
+                        <div className={style.stickyWrapper}>
+                            {/* ------------------------- HEADER -------------------------  */}
+                            <div className={style.header}>
+                                <div>
+                                    <h2>{product.details.name}</h2>
+                                    {Number(product.details.price) > 50 && (
+                                        <span>Free Shipping</span>
+                                    )}
+                                </div>
+                                <div>
+                                    <span>
+                                        {getStars(
+                                            product.reviews_counter,
+                                            product.rating
+                                        )}
+                                    </span>
+                                    <span>
+                                        (
+                                        <Link href='/'>
+                                            {product.reviews_counter} Reviews
+                                        </Link>
+                                        )
+                                    </span>
+                                </div>
+                            </div>
+                            {/* ------------------------- FOR WIDE SCREENS -------------------------  */}
+                            {content}
+                        </div>
                     </div>
                 </div>
                 <ProductRows
