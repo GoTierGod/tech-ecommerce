e2047904233a7a6c7bbd6f3138e65699f7d0ddb9
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-06-21 20:32:43 -0400

    initial structure and style for the search page

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -2,19 +2,44 @@ import ProductRows from '@/components/ProductRows'
 import style from './page.module.css'
 
 import { getData } from '@/helpers/getData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortAmountDesc, faTasks } from '@fortawesome/free-solid-svg-icons'
+import { CardProductDetails } from '@/types/products'
+import VerticalCard from '@/components/VerticalCard'
 
 export default async function Search({ params }: { params: { slug: string } }) {
     const { slug: search } = params
     const cleanedSearch = search.replace(/(\%20)+/g, ',')
 
-    const products = await getData(
+    const products: CardProductDetails[] = await getData(
         `https://ft-drf-api.vercel.app/api/search/${cleanedSearch}`
     )
 
     return (
         <main>
             <div className={style.wrapper}>
-                <h1>{cleanedSearch}</h1>
+                <div className={style.header}>
+                    <div>
+                        <button>
+                            <FontAwesomeIcon icon={faSortAmountDesc} /> Sort
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faTasks} /> Filter
+                        </button>
+                    </div>
+                    <h2>{search}</h2>
+                </div>
+                <div className={style.results}>
+                    <h3>1.476 Results</h3>
+                    <div className={style.grid}>
+                        {products.map(product => (
+                            <VerticalCard
+                                key={product.details.id}
+                                product={product}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </main>
     )
