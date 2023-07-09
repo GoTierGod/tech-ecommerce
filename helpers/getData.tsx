10266d606a1f1b484cd71c081f7e68b6745410dc
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-07-09 13:59:14 -0400

    getData now revalidates every 60 seconds

--- helpers/getData.tsx
+++ helpers/getData.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation'
 
 // FETCH WITH ERROR HANDLING FOR API CALLS
 export const getData = async (url: string) => {
-    const res = await fetch(url)
+    const res = await fetch(url, { next: { revalidate: 60 } })
 
     if (!res.ok) return notFound()
 
