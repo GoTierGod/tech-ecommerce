c8634d4085f090c52ce5cf17466a892e95191014
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-07-09 11:50:18 -0400

    personalized base not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import style from './not-found.module.css'
+
+export default function NotFound() {
+    return (
+        <main className={style.notFound}>
+            <h1>404</h1>
+            <span></span>
+            <h2>Not Found</h2>
+        </main>
+    )
+}
