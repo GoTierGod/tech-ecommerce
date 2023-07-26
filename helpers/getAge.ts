a805c305fd8c9d228f096c61c977a20d4d73bf4b
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-07-26 15:53:12 -0400

    getAge helper function, get the age of an user using the date format yyyy-mm-dd

---
+++ helpers/getAge.ts
@@ -0,0 +1,21 @@
+export const getAge = (birthdate: string): number => {
+    const birthYear = parseInt(birthdate.split('-')[0], 10)
+    const birthMonth = parseInt(birthdate.split('-')[1], 10) - 1
+    const birthDay = parseInt(birthdate.split('-')[2], 10)
+
+    const today = new Date()
+    const todayYear = today.getFullYear()
+    const todayMonth = today.getMonth()
+    const todayDay = today.getDate()
+
+    let age = todayYear - birthYear
+
+    if (
+        todayMonth < birthMonth ||
+        (todayMonth === birthMonth && todayDay < birthDay)
+    ) {
+        age--
+    }
+
+    return age
+}
