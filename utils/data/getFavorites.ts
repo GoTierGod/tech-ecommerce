af026f896ffe1380dd44746018faad394188d12b
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-08-18 19:48:10 -0400

    getFavorites util function

---
+++ utils/data/getFavorites.ts
@@ -0,0 +1,37 @@
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/dist/client/components/headers'
+
+import { API_URL } from '@/constants/api'
+import { ComposedProductInfo } from '@/types/product'
+import { AuthTokens } from '@/types/tokens'
+
+export const getFavorites = async (): Promise<ComposedProductInfo[] | null> => {
+    const authCookies = cookies().get('authTokens')
+
+    if (authCookies) {
+        let authTokens: AuthTokens | null = null
+        try {
+            authTokens = JSON.parse(authCookies.value)
+        } catch (err) {
+            redirect('/')
+        }
+
+        if (authTokens) {
+            const res = await fetch(`${API_URL}/api/favorites/`, {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authTokens.access}`
+                }
+            })
+
+            if (res.ok) {
+                return await res.json()
+            }
+
+            return null
+        }
+    }
+
+    return null
+}
