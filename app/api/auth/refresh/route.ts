ab7463c9a82e6fb3776981e09b051099c13a1f74
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-08-18 01:05:28 -0400

    improved auth/refresh route handler algorithm and comments to explain it

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -6,51 +6,84 @@ import { AuthTokens } from '@/types/tokens'
 import { API_URL } from '@/constants/api'
 
 export async function GET(req: NextRequest) {
+    // Get search params
     const { searchParams } = new URL(req.url)
     const path = searchParams.get('path')
     const auth = searchParams.get('auth')
-    if (!path) redirect('/')
 
+    // Check for auth cookies
     const authCookies = cookies().get('authTokens')
     if (!authCookies) redirect('/')
 
-    const authTokens: AuthTokens = JSON.parse(authCookies.value)
-    if (!authTokens.refresh) redirect('/')
+    // Check for path
+    if (!path) {
+        cookies().delete('authTokens')
+        redirect('/')
+    }
 
+    // Check if auth cookies is valid JSON
+    let authTokens: AuthTokens | null = null
+    try {
+        authTokens = JSON.parse(authCookies.value)
+    } catch (err) {
+        cookies().delete('authTokens')
+    }
+    // Check if there's a refresh token in cookies
+    if (!authTokens?.refresh) redirect('/')
+
+    // Try to refresh the user auth tokens
     const refreshData: RefreshRequestData = {
         refresh: authTokens.refresh
     }
 
     const res = await fetch(`${API_URL}/api/token/refresh/`, {
         method: 'post',
-        cache: 'no-cache',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(refreshData)
     })
 
+    // If authentication matters
     if (auth) {
-        if (!['0', '1'].includes(auth)) redirect('/')
+        // If the auth parameter is not 0 or 1, delete the cookies and redirect
+        if (!['0', '1'].includes(auth)) {
+            cookies().delete('authTokens')
+            redirect('/')
+        }
 
+        // If the request was sucessfull, set the new auth tokens and
+        //     if the route needs authentication, redirect to the route which call this route handler
+        //     otherwise redirect tothe home page
         if (res.ok) {
             const data = await res.json()
             cookies().set('authTokens', JSON.stringify(data))
 
             if (Boolean(Number(auth))) redirect(`${path}`)
             else redirect('/')
-        } else {
+        }
+        // Otherwise delete invalid cookies and
+        //     if this route needs authentication, redirect to /login
+        //     otherwise redirect to the route which call this route handler
+        else {
             cookies().delete('authTokens')
 
             if (Boolean(Number(auth))) redirect(`/login`)
             else redirect(`${path}`)
         }
-    } else {
+    }
+    // If authentication does not matter
+    else {
+        // If the request was successfull, set the new auth tokens
         if (res.ok) {
             const data = await res.json()
             cookies().set('authTokens', JSON.stringify(data))
-        } else cookies().delete('authTokens')
+        }
+        // Otherwise delete the invalid auth tokens
+        else cookies().delete('authTokens')
 
+        // Finally as authentication does not matter
+        //     redirect to the route which call this route handler
         redirect(`${path}`)
     }
 }
