420a14b67bcc53ae4fd26303484fe00cb553d119
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-08-20 10:37:29 -0400

    route handlers for create, update and delete cart and favorites items

---
+++ app/api/favorites/add/route.ts
@@ -0,0 +1,46 @@
+import { API_URL } from '@/constants/api'
+import { APIResponse } from '@/types/api-response'
+import { AuthTokens } from '@/types/tokens'
+import { cookies } from 'next/dist/client/components/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+    try {
+        const authCookies = cookies().get('authTokens')
+
+        if (authCookies) {
+            let authTokens: AuthTokens | null = null
+            try {
+                authTokens = JSON.parse(authCookies.value) as AuthTokens
+            } catch (err) {
+                cookies().delete('authTokens')
+
+                return NextResponse.json(
+                    { message: 'Invalid tokens' },
+                    { status: 401 }
+                )
+            }
+
+            const { searchParams } = new URL(req.url)
+            const id = searchParams.get('id')
+
+            const res = await fetch(`${API_URL}/api/favorites/create/${id}`, {
+                method: 'POST',
+                headers: { authorization: `Bearer ${authTokens.access}` }
+            })
+
+            if (res.ok) {
+                const apiResponse: APIResponse = await res.json()
+                return NextResponse.json(apiResponse, { status: 200 })
+            }
+
+            const errorResponse: APIResponse = await res.json()
+            return NextResponse.json(errorResponse, { status: res.status })
+        }
+    } catch (err) {
+        return NextResponse.json(
+            { message: 'Something went wrong' },
+            { status: 400 }
+        )
+    }
+}
