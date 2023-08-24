f7a2a9b8c9f71aeb24b503817f0c1ebdc4f1bb75
GoTierGod <ivan.gunlove_10_@hotmail.com>
2023-08-24 05:42:10 -0400

    correct validation path when redirecting within purchase routes

--- app/purchase/cart/page.tsx
+++ app/purchase/cart/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation'
 
 export default async function Page() {
     const user = await getUser()
-    if (!user) redirect('/api/auth/refresh?path=/cart&auth=1')
+    if (!user) redirect('/api/auth/refresh?path=/purchase/cart&auth=1')
 
     const cart = await getCart()
     if (!cart) redirect('/')
--- app/purchase/product/[id]/page.tsx
+++ app/purchase/product/[id]/page.tsx
@@ -4,13 +4,14 @@ import { getUser } from '@/utils/data/getUser'
 import { redirect } from 'next/navigation'
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const user = await getUser()
-    if (!user) redirect('/api/auth/refresh?path=/cart&auth=1')
-
     const { id } = params
+
+    const user = await getUser()
+    if (!user) redirect(`/api/auth/refresh?path=/purchase/product/${id}&auth=1`)
+
     const product = await getProduct(id)
 
     if (!product) redirect('/')
 
-    return <Purchase order={[product]} />
+    return <Purchase customer={user} order={[product]} />
 }
