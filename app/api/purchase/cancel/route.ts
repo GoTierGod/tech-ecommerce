9daa296cd0a5530c64aba8c7c852b0fdca234f33
GoTierGod <ivan.gunlove_10_@hotmail.com>
2023-08-28 02:24:28 -0400

    purchase cancel and update route handlers

---
+++ app/api/purchase/cancel/route.ts
@@ -0,0 +1,49 @@
+import { API_URL } from '@/constants/api'
+import { APIResponse } from '@/types/api-response'
+import { AuthTokens } from '@/types/tokens'
+import { cookies } from 'next/dist/client/components/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+    try {
+        const authCookies = cookies().get('authTokens')
+
+        if (authCookies) {
+            let authTokens: AuthTokens | null = null
+            try {
+                authTokens = JSON.parse(authCookies.value) as AuthTokens
+            } catch (err) {
+                cookies().delete('authTokens')
+
+                return NextResponse.json(
+                    { message: 'Invalid tokens' },
+                    { status: 401 }
+                )
+            }
+
+            const { searchParams } = new URL(req.url)
+            const id = searchParams.get('id')
+
+            const res = await fetch(`${API_URL}/api/purchase/delete/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${authTokens.access}`
+                }
+            })
+
+            if (res.ok) {
+                const apiResponse: APIResponse = await res.json()
+                return NextResponse.json(apiResponse, { status: 200 })
+            }
+
+            const errorResponse: APIResponse = await res.json()
+            return NextResponse.json(errorResponse, { status: res.status })
+        }
+    } catch (err) {
+        return NextResponse.json(
+            { message: 'Something went wrong' },
+            { status: 400 }
+        )
+    }
+}
