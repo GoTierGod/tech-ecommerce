93c6c1662d857eb6f4bc41ef7f7a2e3252434868
GoTierGod <ivan.gunlove_10_@hotmail.com>
2023-09-08 06:57:28 -0300

    updated API route in getReviews function

--- utils/data/getReviews.ts
+++ utils/data/getReviews.ts
@@ -2,7 +2,7 @@ import { API_URL } from '@/constants/back-end'
 import { ComposedReviewInfo } from '@/types/review'
 
 export const getReviews = async (id: string): Promise<ComposedReviewInfo[]> => {
-    const res = await fetch(`${API_URL}/api/reviews/${id}`, {
+    const res = await fetch(`${API_URL}/api/reviews/product/${id}`, {
         next: { revalidate: 3600 }
     })
 
