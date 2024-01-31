8b250e77b82f4a42ec1238c9dac343167e7db05a
GoTierGod <ivan.gunlove_10_@hotmail.com>
2024-01-31 06:08:51 -0300

    New icons, miniature and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,31 @@ import { getData } from '@/utils/data/getData'
 
 const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 
+const description = `This front-end application built with Next.js 13 and was 
+seamlessly integrated with the Django REST Framework API. This synergy 
+creates an easy-to-use graphical interface (GUI) that enhances the user 
+experience and allows the user to fully explore all of the website's features.`
+
 export const metadata: Metadata = {
     title: 'Tech',
-    description: 'Tech specialized E-Commerce project made by @GoTierGod.',
+    description: description,
+    metadataBase: new URL('https://tech-ecommerce-ebon.vercel.app/'),
+    creator: 'GoTierGod',
+    publisher: 'GoTierGod',
+    authors: {
+        name: 'GoTierGod',
+        url: 'https://tech-ecommerce-ebon.vercel.app/'
+    },
+    robots: 'index, follow',
+    twitter: {
+        title: 'Tech',
+        description: description,
+        images: ['/twitter-image.png']
+    },
     openGraph: {
-        images: ['/min.svg', '/min.webp']
+        title: 'Tech',
+        description: description,
+        images: ['/opengraph-image.png']
     }
 }
 
